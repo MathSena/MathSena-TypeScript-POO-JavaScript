cd4d60f7292cad0dc3731e269888a153e076caad
MathSena <MathSena07@hotmail.com>
2021-04-11 19:58:16 -0300

    Inferência de tipos

---
+++ 3.Inferência de tipos/app.ts
@@ -0,0 +1,17 @@
+/*
+No Typescript ele faz validação de tipos
+*/
+
+let mensagem: string = "Seja bem-vindo!!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
+let notasDasProvas: Array<number> = [7.5,8,9]
+let listaDeFrutas: Array<String> = ['Uva', 'Banana', 'Abacaxi']
+let listaDeFrutas2: string[] = ['Uva', 'Banana', 'Abacaxi']
+
+
+
+
+
+
+
