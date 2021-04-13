46d43a69d8d8fdb5dcc353db96b438c9ae087c7f
MathSena <MathSena07@hotmail.com>
2021-04-12 22:13:30 -0300

    Criando classe Pessoa

--- 4.Orientação a Objetos/app.js
+++ 4.Orientação a Objetos/app.js
@@ -15,9 +15,43 @@ var Carro = /** @class */ (function () {
     };
     return Carro;
 }());
+var Concessionaria = /** @class */ (function () {
+    function Concessionaria(endereco) {
+        this.endereco = this.endereco;
+    }
+    Concessionaria.prototype.fornecerEndereco = function () {
+        return this.endereco;
+    };
+    Concessionaria.prototype.mostrarListaDeCarro = function () {
+        return this.listaDeCarros;
+    };
+    return Concessionaria;
+}());
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
+    };
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    return Pessoa;
+}());
 var carroB = new Carro('Ferrari', 4);
 console.log(carroB);
 carroB.acelerar();
 console.log(carroB);
 carroB.acelerar();
-console.log(carroB);
+var concessionariaA = new Concessionaria('Avenida Paulista');
+console.log(concessionariaA.fornecerEndereco());
+var pessoaA = new Pessoa('Matheus', 'Jeep Renegade');
+console.log(pessoaA.dizerCarroPreferido());
--- 4.Orientação a Objetos/app.ts
+++ 4.Orientação a Objetos/app.ts
@@ -38,19 +38,41 @@ class Concessionaria{
     }
 }
 
+class Pessoa{
+    private nome:string
+    private carroPreferido:string
+    private carro:any
 
+    constructor(nome:string, carroPreferido:string){
+        this.nome = nome
+        this.carroPreferido = carroPreferido
+    }
 
+    public dizerNome():string{
+        return this.nome
+    }
 
+    public dizerCarroPreferido():string{
+        return this.carroPreferido
+    }
 
+    public comprarCarro(carro:any):void{
+         this.carro = carro
+    }
 
-
-
-
-
-
+    public dizerCarroQueTem():any{
+        return this.carro
+    }
+}
 
 let carroB = new Carro('Ferrari', 4)
 console.log(carroB)
 carroB.acelerar()
 console.log(carroB)
 carroB.acelerar()
+
+let concessionariaA = new Concessionaria('Avenida Paulista')
+console.log(concessionariaA.fornecerEndereco())
+
+let pessoaA = new Pessoa('Matheus', 'Jeep Renegade')
+console.log(pessoaA.dizerCarroPreferido())
